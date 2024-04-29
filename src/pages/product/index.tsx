d968f946f6df0467f8b14e6ac7b5c5ab6fa829f9
pancaholic <putrapanca21@gmail.com>
2024-04-29 12:50:54 +0700

    fetching api product from db

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,19 +1,45 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
+
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [product, setProduct] = useState([]);
   const { push } = useRouter();
 
   useEffect(() => {
     if (!isLogin) {
       push("/auth/login");
     }
+  }, [isLogin, push]);
+
+  useEffect(() => {
+    fetch("api/product")
+      .then((res) => res.json())
+      .then((allData) => {
+        setProduct(allData.data);
+      });
   }, []);
-  
+
   return (
     <div>
       <h1>Product Page</h1>
+      {product.map((item: productType) => (
+        <div
+          key={item.id}
+          className="flex flex-col items-center justify-center"
+        >
+          <h1>{item.name}</h1>
+          <p>{item.price}</p>
+          <p>{item.size}</p>
+        </div>
+      ))}
     </div>
   );
 };
